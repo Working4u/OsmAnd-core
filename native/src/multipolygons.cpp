76d339148d42dc4b95b8465094df0d1dcd7d157a
MadWasp79 <madwasp79@gmail.com>
2019-10-24 11:26:26 +0300

    test_setting typo fix

--- native/src/multipolygons.cpp
+++ native/src/multipolygons.cpp
@@ -318,7 +318,7 @@ void unifyIncompletedRings(std::vector<std::vector<int_pair> >& toProccess, std:
 		// int EVAL_DELTA = 6 << (23 - zoom);
 		// const int UNDEFINED_MIN_DIFF = -1 - EVAL_DELTA;
 		int EVAL_DELTA = 0;
-		int UNDEFUNED_MIN_DIFF = 0;
+		int UNDEFINED_MIN_DIFF = 0;
 
 		while (true) {
 			int st = 0; // st already checked to be one of the four
